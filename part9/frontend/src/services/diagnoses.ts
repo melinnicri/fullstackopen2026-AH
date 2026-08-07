3a0cf840f3dbc17c2c6ec5a25320b4be8cf4168e
Amelia Cristina Herrera Briceño <123215286+melinnicri@users.noreply.github.com>
2026-08-06 21:32:59 -0400

    Delete part9 directory
    borrar part9

--- part9/frontend/src/services/diagnoses.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { Diagnosis } from "../types";
-
-const baseUrl = "/api/diagnoses";
-
-export const getAll = async (): Promise<Diagnosis[]> => {
-    const { data } = await axios.get<Diagnosis[]>(baseUrl);
-    return data;
-};
